aa9e5746401bc6e9e3b1d91914c83bc297441cca
Lucasx10 <lukasprado260@gmail.com>
2024-06-04 10:16:39 -0400

    Adicionando url para home

--- healing/urls.py
+++ healing/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,4 +9,5 @@ urlpatterns = [
     path('usuarios/', include('usuarios.urls')),
     path('medicos/', include('medico.urls')),
     path('pacientes/', include('paciente.urls')),
+    path('', lambda request: redirect('/usuarios/login'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
