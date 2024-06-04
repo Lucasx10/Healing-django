7e51e6b84d272496963f69ef07f9d191afbf706b
Lucasx10 <lukasprado260@gmail.com>
2024-06-04 10:46:33 -0400

    Adiciona botao de cancelar consulta

--- paciente/urls.py
+++ paciente/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('agendar_horario/<int:id_data_aberta>/', views.agendar_horario, name="agendar_horario"),
     path('minhas_consultas/', views.minhas_consultas, name="minhas_consultas"),
     path('consulta/<int:id_consulta>/', views.consulta, name="consulta"),
+    path('cancelar_consulta/<int:id_consulta>/', views.cancelar_consulta, name="cancelar_consulta"),
 ]
--- paciente/views.py
+++ paciente/views.py
@@ -22,14 +22,14 @@ def home(request):
         especialidades = Especialidades.objects.all()
         return render(request, 'home.html', {'medicos':medicos, 'especialidades':especialidades, 'is_medico': is_medico(request.user)})
 
-login_required
+@login_required
 def escolher_horario(request, id_dados_medicos):
     if request.method == 'GET':
         medico = DadosMedico.objects.get(id=id_dados_medicos)
         datas_abertas = DatasAbertas.objects.filter(user=medico.user).filter(data__gte=datetime.now()).filter(agendado= False)
         return render(request, 'escolher_horario.html', {'medico':medico, 'datas_abertas': datas_abertas, 'is_medico': is_medico(request.user)})
 
-login_required    
+@login_required    
 def agendar_horario(request, id_data_aberta):
     if request.method == 'GET':
         data_aberta = DatasAbertas.objects.get(id=id_data_aberta)
@@ -48,7 +48,7 @@ def agendar_horario(request, id_data_aberta):
         
         return redirect('/pacientes/minhas_consultas')
 
-login_required    
+@login_required    
 def minhas_consultas(request):
         data = request.GET.get("data")
         especialidade = request.GET.get("especialidade")
@@ -64,10 +64,21 @@ def minhas_consultas(request):
 
         return render(request, 'minhas_consultas.html', {'minhas_consultas': minhas_consultas, 'especialidades':especialidades, 'is_medico': is_medico(request.user) })
 
-login_required
+@login_required
 def consulta(request, id_consulta):
     if request.method == 'GET':
         consulta = Consulta.objects.get(id=id_consulta)
         dado_medico = DadosMedico.objects.get(user=consulta.data_aberta.user)
         documentos = Documento.objects.filter(consulta=consulta)
         return render(request, 'consulta.html', {'consulta': consulta, 'dado_medico': dado_medico, 'documentos': documentos, 'is_medico': is_medico(request.user) })
+
+@login_required    
+def cancelar_consulta(request, id_consulta): 
+    consulta = Consulta.objects.get(id=id_consulta)
+    if request.user != consulta.paciente:
+        messages.add_message(request, constants.ERROR, 'Essa consulta não é sua')
+        return redirect(f'/pacientes/home/')
+    
+    consulta.status = 'C'
+    consulta.save()
+    return redirect(f'/pacientes/consulta/{id_consulta}')
